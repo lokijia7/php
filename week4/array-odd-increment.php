cee0e5482ab1ea56bed597ff60bc23e00c4834b3
LOK JIA QI <jiaqi213006@dasein.edu.my>
2023-03-09 18:00:09 +0800

    q1

--- week4/array-odd-increment.php
+++ week4/array-odd-increment.php
@@ -1,3 +1,23 @@
-$odd_numbers = array(); // Create an empty array to store the odd numbers
+<!DOCTYPE html>
+<html>
 
-for ($i = 1; $i <= 100; $i++) { // Loop through the numbers from 1 to 100 if ($i % 2 !==0) { // If the number is odd array_push($odd_numbers, $i); // Add it to the array } } print_r($odd_numbers); // Print the array of odd numbers
+<body>
+
+    <?php
+    $odd_numbers = array();  // Create an empty array to store the odd numbers
+
+    for ($i = 1; $i <= 100; $i++) {  // Loop through the numbers from 1 to 100
+        if ($i % 2 !== 0) {  // If the number is odd
+            array_push($odd_numbers, $i);  // Add it to the array
+        }
+    }
+
+    print_r($odd_numbers);  // Print the array of odd numbers
+
+    ?>
+
+
+
+</body>
+
+</html>
