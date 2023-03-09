b7fa15f29c4bf80c54fc2be4e478f8520ce782d8
LOK JIA QI <jiaqi213006@dasein.edu.my>
2023-03-09 20:39:30 +0800

    q2

---
+++ week4/array-double-increment.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+
+    $arr = array(1);
+    for ($i = 1; $i < 10; $i++) {
+        $arr[$i] = $arr[$i - 1] * 2;
+    }
+
+
+
+    ?>
+
+
+
+</body>
+
+</html>
