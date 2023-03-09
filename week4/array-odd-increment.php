97d5770197cdfd913c7ef2443e6706aacf4c0cae
LOK JIA QI <jiaqi213006@dasein.edu.my>
2023-03-09 18:15:12 +0800

    q1

--- week4/array-odd-increment.php
+++ week4/array-odd-increment.php
@@ -4,19 +4,17 @@
 <body>
 
     <?php
-    // initialize an empty array to store the odd numbers
+
     $odd_nums = array();
 
-    // use a for loop to iterate over the range of numbers from 1 to 100
     for ($i = 1; $i <= 100; $i++) {
-        // check if the current number is odd
+
         if ($i % 2 != 0) {
-            // if it is, add it to the $odd_nums array
+
             $odd_nums[] = $i;
         }
     }
 
-    // use a for loop to iterate over the odd_nums array and display its elements with the desired format
     for ($i = 0; $i < count($odd_nums); $i++) {
         echo ($i + 1) . "st element - " . $odd_nums[$i] . "<br>";
     }
