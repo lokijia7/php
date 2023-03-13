37c8981d61e5c580f1c44ac7b83cc0ce5acff418
LOK JIA QI <jiaqi213006@dasein.edu.my>
2023-03-13 19:27:20 +0800

    q2

--- week4/array-double-increment.php
+++ week4/array-double-increment.php
@@ -4,16 +4,11 @@
 <body>
 
     <?php
-    $arr = array();
-    $num = 1;
-    for ($i = 1; $i <= 10; $i++) {
-        $arr[] = $num;
-        $num *= 2;
+    $array = [1];
+    for ($i = 1; $i < 10; $i++) {
+        $array[$i] = $array[$i - 1] * 2;
     }
 
-    foreach ($arr as $key => $value) {
-        echo ($key + 1) . "st element - " .  $value . "<br>";
-    }
     ?>
 
 
